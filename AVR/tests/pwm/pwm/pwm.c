2782bbb958dedc28aa5154f267b5d2c55ab374ef
jcedergren <joakim.cedergren1@gmail.com>
2019-12-03 16:25:35 +0100

    Added an inverted PWM test. Must be tested on the AVR.

---
+++ AVR/tests/pwm/pwm/pwm.c
@@ -0,0 +1,34 @@
+/*
+ * pwm.c
+ *
+ *
+ *	Sends an inverted PWM over pin PD5 and PD6
+ *
+ * Created: 2019-12-03 16:20:52
+ *  Author: joakimcedergren
+ */ 
+
+
+#include <avr/io.h>
+
+#define PWM 100
+
+static void setup(){
+	
+	DDRD |= (1 << PD6) | (1 << PD5);
+	TCCR0A = (1 << COM0A1) | (1 << COM0A0) | (1 << COM0B1) | (1 << COM0B0) | (1 << WGM01) | (1 << WGM00);
+	TCCR0B =  (0 << CS02) | (0 << CS01) | (1 << CS00);
+
+}
+
+int main(void){
+    
+	setup();
+	
+	OCR0A = PWM;
+	OCR0B = PWM;
+	
+	while(1){
+        
+    }
+}
